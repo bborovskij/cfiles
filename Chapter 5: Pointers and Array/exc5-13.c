0672b0d95805b319d0ccd325b9b48146d13a4a72
bborovskij <48993099+bborovskij@users.noreply.github.com>
2021-07-29 13:46:52 +0300

    Update exc5-13.c

--- Chapter 5: Pointers and Array/exc5-13.c
+++ Chapter 5: Pointers and Array/exc5-13.c
@@ -4,9 +4,9 @@ Exercise 5-13. Write the program tail, which prints the last n lines of its inpu
 
 $ tail -n
 
-prints the last n lines. The program should behave rationally no matter how unreasonable 
-the input or the value of n. Write the program so it makes the best use of available storage; 
- lines should be stored as in the sorting program of Section 5.6, not in a two-dimensional 
+prints the last n lines. The program should behave rationally no matter how unreasonable
+the input or the value of n. Write the program so it makes the best use of available storage;
+ lines should be stored as in the sorting program of Section 5.6, not in a two-dimensional
  array of fixed size.
  */
 #include <stdio.h>
@@ -37,15 +37,21 @@ int read_and_add_to_buffer(char *buffer) {
 }
 
 int parse_line_nums(const int *argc, char *argv[]) {
-    int linenum;
+    int linenum = 10;
     if (*argc == 1) {
-        return 0;
+        return linenum;
     } else if (*argc != 2 && *argc != 3) {
         printf("Accepts only -n=<number> or -n <number>");
-        return 0;
+        return -1;
     }
+
     if (!strcmp(argv[1], "-n")) {
-        linenum = (int) strtod(argv[2], NULL);
+        if (*argc == 3) {
+            linenum = (int) strtod(argv[2], NULL);
+        } else {
+            printf("Accepts only -n=<number> or -n <number>");
+            return -1;
+        }
     } else {
         for (; *argv[1] != '='; ++argv[1]) {};
         ++argv[1];
@@ -73,8 +79,9 @@ int main(int argc, char *argv[]) {
     total_lines = read_and_add_to_buffer(main_buffer);
     lines_num = parse_line_nums(&argc, argv);
 
-    if (!lines_num) {
-        printf("%s", main_buffer);
+    if (lines_num <= 0) {
+        printf("Error parsing arguments: argc=%d, -n=%d\n.", argc, lines_num);
+        return 1;
     } else {
         print_last_lines(main_buffer, &lines_num, &total_lines);
     }
